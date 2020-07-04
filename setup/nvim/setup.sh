5b7d2cc66aad33bfdf3a085d41759d21c749e666
ChocolateOverflow <57666296+ChocolateOverflow@users.noreply.github.com>
2020-07-04 15:10:01 +0700

    Removed legion; Updated setup(nvim)

--- setup/nvim/setup.sh
+++ setup/nvim/setup.sh
@@ -5,19 +5,28 @@ source var.sh
 
 echo -e "[START] Installing and setting up stuff for Vim\n"
 
-mkdir -p $path_to_vim/autoload $path_to_vim/bundle $path_to_vim/colors $path_to_vim/plugged
+mkdir -p $path_to_vim/autoload $path_to_vim/plugged
 
 # Install stuff with package manager
-#sudo pacman -S --needed --noconfirm vim-runtime
+sudo pacman -S --needed --noconfirm vim-runtime
 
 # Install vim-plug
 curl "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" > $path_to_vim/autoload/plug.vim
 
+# Dependencies
+python3 -m pip install black pynvim
+
 # Install all the plugins
-vim --cmd ":source $path_to_dotfiles/home/.vimrc" \
+vim --cmd ":source $path_to_vim/init.vim" \
 	-c ":PlugInstall" \
-	-c ":CocInstall coc-snippets" \
-	-c ":CocInstall coc-json" \
 	-c ":qa!" 2>/dev/null
+
+# vim-jsbeautify
+cp ./.editorconfig $path_to_vim/
+
+# sym-link vim to nvim
+ln -s $HOME/.config/nvim/init.vim $HOME/.vimrc
+ln -s $HOME/.config/nvim/ $HOME/.vim
+sudo ln -s /usr/bin/nvim /usr/bin/vim
 
 echo -e "[DONE] Installing and setting up stuff for Vim\n"
